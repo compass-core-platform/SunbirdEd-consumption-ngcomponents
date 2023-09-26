ca3e69ed41e47ec2005dd373fcafac26f25c8ac1
pavankumar0408 <131791648+pavankumar0408@users.noreply.github.com>
2023-09-26 08:52:35 +0530

    Icon and alignment changes (#26)

--- projects/common-consumption/src/lib/content-details/toc-content/toc-content.component.ts
+++ projects/common-consumption/src/lib/content-details/toc-content/toc-content.component.ts
@@ -12,6 +12,21 @@ export class TocContentComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.getIcons();
+  }
+
+  getIcons() {
+    if(this.courseModules.body.length > 0) {
+      this.courseModules.body.forEach((course) => {
+        if(course.mimeType == 'video/mp4') {
+          course.mimeType = 'Video'
+        } else if(course.mimeType == 'application/vnd.ekstep.content-collection' || course.mimeType == 'application/vnd.ekstep.ecml-archive') {
+          course.mimeType = 'Document'
+        } else if(course.mimeType == 'application/vnd.sunbird.questionset') {
+          course.mimeType = 'Assessment'
+        }
+      })
+    }
   }
 
   selectedContent(event: any, content: any) {
