3194716160ac9d8129965512d97e2cd41c7b95fb
Shoaib Mohmad <shoaib.mohmad@tarento.com>
2023-10-04 11:21:57 +0530

    Course card update (#28)
    * Updated course card to show images.
    
    * Created & displayed progress bar & course duration
    
    ---------
    
    Co-authored-by: shoaib_mohmad <shoaibmohammad@Netopss-MacBook-Pro.local>

--- projects/common-consumption/src/lib/card/compass-course-card/compass-course-card.component.ts
+++ projects/common-consumption/src/lib/card/compass-course-card/compass-course-card.component.ts
@@ -12,6 +12,10 @@ export class CompassCourseCardComponent implements OnInit {
   name: string;
   category: string;
   image: string = "/assets/images/course-1.png";
+  showProgress: boolean = false;
+  completionPercentage: any;
+  contentType:string;
+  duration:string;
 
   constructor() { }
 
@@ -28,6 +32,10 @@ export class CompassCourseCardComponent implements OnInit {
       if (this.data['posterImage'] !== undefined) {
         this.image = this.data['posterImage'];
       }
+      this.contentType = this.data['contentType'];
+      if (this.data['Duration'] !== undefined) {
+        this.duration = this.data['Duration'];
+      }
     } else {
       this.name = this.data['courseName'];
       if (this.data['content']["subject"] !== undefined) {
@@ -40,7 +48,31 @@ export class CompassCourseCardComponent implements OnInit {
       if (this.data['content']['posterImage'] !== undefined) {
         this.image = this.data['content']['posterImage'];
       }
+      if (this.data['completionPercentage'] !== undefined) {
+        this.showProgress = true;
+        this.completionPercentage = this.data['completionPercentage'];
+      }
+      this.contentType = this.data['content']['contentType'];
+      if (this.data['content']['Duration'] !== undefined) {
+        this.image = this.data['content']['Duration'];
+      }
     }
+  }
+
+  fetchProgress() {
+    var bgColor = "#024f9d";
+    // var endDate = (this.course.batch!=null && this.course.batch.endDate!=null)?new Date(this.course.batch.endDate):null;
+    if(this.completionPercentage == "100") {
+      bgColor = "#07bc81"; // green
+    } 
+    // else if (endDate!=null && endDate < new Date()) {
+    //   bgColor = "#ff4558"; // red
+    // }
+    let widthStyle = this.completionPercentage+"%";
+    return {
+      width: widthStyle,
+      'background-color':bgColor
+    };
   }
 
 }
